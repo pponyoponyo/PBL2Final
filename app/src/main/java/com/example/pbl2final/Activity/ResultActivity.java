111dab53975fb5afbf3441155837c8d96f2a2af6
BOMIN9594 <kbm17@naver.com>
2019-12-04 04:04:21 +0900

    프로그래스바 연동 전!!333 + 진행률 애니메이션 추가

--- app/src/main/java/com/example/pbl2final/Activity/ResultActivity.java
+++ app/src/main/java/com/example/pbl2final/Activity/ResultActivity.java
@@ -1,8 +1,11 @@
 package com.example.pbl2final.Activity;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.pbl2final.R;
@@ -10,7 +13,12 @@ import com.example.pbl2final.R;
 
 public class ResultActivity extends BaseActivity {
 
-    private TextView txtResult;
+    Handler handler = new Handler() ; // Thread에서 화면에 그리기 위해 필요
+    private TextView txtResult; //
+    private int testNum=9; // 연동 전 임의로 테스트 넘버 넣음
+    private int value=0;
+    private ProgressBar progressBar;
+    private int add=5; // 증가량, 방향
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +26,43 @@ public class ResultActivity extends BaseActivity {
         setContentView(R.layout.activity_result);
 
         txtResult = findViewById(R.id.txtResult);
+        testNum=testNum*10;
+        txtResult.setText(testNum+"%"); // 연동 전 테스트 넘버
 
-        // todo 리턴되는 true 개수를 progressBar.setProgress(trueNum) 해주기
+        // 약간 수정 필요함!!
+        final ProgressBar progressBar=(ProgressBar)findViewById(R.id.progress_circular1);
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(value<=testNum){
+                    value=value+add;
+                    if(value>=100 || value<=0){
+                        add=-add;
+                    }
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setProgress(value);
+                        }
+                    });
+                    try{
+                        Thread.sleep(100); // tlrks wldus
+                    } catch (InterruptedException e){}
+                }
+
+            }
+        });
+        t.start();
+
+
+
+
+   /*     // todo 리턴되는 true 개수를 progressBar.setProgress(trueNum) 해주기
+
+        progressBar=(ProgressBar)findViewById(R.id.progress_circular1);
+        progressBar.setProgress(testNum);
+*/
 /*
         Intent i = getIntent();
         String reason = i.getStringExtra("reason");
